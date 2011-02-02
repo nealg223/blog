22ad43f35bcc5fd8531659412a50849b39eb287a
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-02-02 13:25:42 -0600

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'probity' && password == 'neal'
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '976b7abea8c035916b4e83c1281371ea35df1a2034daf4d72928d4f9fb3753bb153280085169cb8392f510b1650ee7e1da5fa2b640e6e7524afaf30c107859ac'
